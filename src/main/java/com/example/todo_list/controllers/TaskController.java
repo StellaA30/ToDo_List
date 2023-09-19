7011af1255a5249d673539e610dbb9db7127e7c4
StellaA30 <56633439+StellaA30@users.noreply.github.com>
2023-09-19 20:18:00 +0100

    adds code for PutMapping

--- src/main/java/com/example/todo_list/controllers/TaskController.java
+++ src/main/java/com/example/todo_list/controllers/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.todo_list.controllers;
 
 
+import com.example.todo_list.models.Priority;
 import com.example.todo_list.models.Task;
 import com.example.todo_list.models.TaskDTO;
 import com.example.todo_list.models.User;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -49,24 +51,19 @@ public class TaskController {
 
     // get all tasks/derived queries
 
-    public ResponseEntity<List<Task>> getAllTask(@RequestParam(required = false, name = "userId") Long userId,
+    public ResponseEntity<List<Task>> getAllTask(@RequestParam(required = false, name = "user_id") Long userId,
                                                  @RequestParam(required = false, name = "complete") Boolean complete){
-
+        // gets complete/incomplete task for specific user
         if(userId != null && complete!=null){
             return new ResponseEntity<>(taskService.getAllTasksByUserIdAndComplete(userId, complete), HttpStatus.OK);
-
+            // gets tasks for specific user
         } else if (userId != null){
             return new ResponseEntity<>(taskService.getAllTaskByUserId(userId), HttpStatus.OK);
         }
-
         // get all tasks -- default request
         List<Task> allTasks = taskService.getAllTasks();
         return new ResponseEntity<>(allTasks, HttpStatus.OK);
     }
-
-
-
-
 
 
     // delete task
@@ -79,6 +76,29 @@ public class TaskController {
 
 
     // update task
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id,
+                                           @RequestBody(required = false) String text,
+                                           @RequestParam(required = false)Priority priority,
+                                           @RequestParam(required = false, name = "due_date")LocalDate dueDate,
+                                           @RequestParam(required = false) Boolean complete){
+        // update task priority
+        if(priority != null){
+            return new ResponseEntity<>(taskService.updateTaskPriority(id, priority), HttpStatus.OK);
+        }
 
+        // update task due date
+        if(dueDate != null){
+            return new ResponseEntity<>(taskService.updateTaskDueDate(id, dueDate), HttpStatus.OK);
+        }
 
+        // update completion status
+        if(complete != null){
+            return new ResponseEntity<>(taskService.updateTaskCompletion(id, complete), HttpStatus.OK);
+        }
+
+        // update text
+        return new ResponseEntity<>(taskService.updateTask(id, text), HttpStatus.OK);
+
+    }
 }
